06a6d2da3310478232346739b6861073a2088325
Siddhant <s1d.official011001011@gmail.com>
2024-10-22 23:29:05 +0530

    adasdada

--- constants.ts
+++ constants.ts
@@ -3,19 +3,9 @@ export const FQDN = "https://api.gokapturehub.com";
 export const API_URL = `${FQDN}/collage-ranking`;
 
 export const SONG_LISTS = [
-    "Enter Sandman",
-    "The Trooper",
-    "Paranoid",
-    "Raining Blood",
-    "Walk",
-    "Holy Wars... The Punishment Due",
-    "Painkiller",
-    "Duality",
-    "Chop Suey!",
-    "Bat Country",
-    "Square Hammer",
-    "Redneck",
-    "Ghost of Perdition",
-    "Pull Me Under",
-    "Twilight of the Thunder God"
-]
+  "Chand Sifarish",
+  "Jabse tere naina",
+  "Dus bahane",
+  "Behti Hawa Sa tha woh",
+  "Where's the party tonight",
+];
